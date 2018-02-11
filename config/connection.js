44b4a7edb2e1195d3f17c2d8865fcea8a647ad72
Karissa McKendall <kmckendall@gmail.com>
2018-02-11 18:46:44 -0500

    updated connection file to Jaws

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,7 @@ console.log("connection.js");
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-
+var connection;
 
 if(process.env.JAWSDB_URL){
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
